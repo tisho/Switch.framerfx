96c781cae4513997761a928873776b24ac878856
Tisho Georgiev <tisho@framer.com>
2019-11-28 09:53:20 -0800

    fix: fixed an issue where an overridden event trigger like onTap wouldn't be called if the on tap trigger wasn't set on the switch instance itself

--- code/extractEventHandlersFromProps.ts
+++ code/extractEventHandlersFromProps.ts
@@ -20,19 +20,19 @@ export function extractEventHandlersFromProps(
     const keyEvents = []
     const eventHandlers = eventTriggerNames.reduce((handlers, event) => {
         const action = props[`${event}Action`]
-        if (action !== "unset") {
-            const handler = (...args) => {
-                // execute any existing handlers
-                // key events are excluded here, because they're handled in a
-                // separate event handler
-                if (
-                    keyEventTriggerNames.indexOf(event) === -1 &&
-                    props[event] &&
-                    typeof props[event] === "function"
-                ) {
-                    props[event](...args)
-                }
+        const handler = (...args) => {
+            // execute any existing handlers
+            // key events are excluded here, because they're handled in a
+            // separate event handler
+            if (
+                keyEventTriggerNames.indexOf(event) === -1 &&
+                props[event] &&
+                typeof props[event] === "function"
+            ) {
+                props[event](...args)
+            }
 
+            if (action !== "unset") {
                 // check if a trigger condition has been passed in
                 if (
                     "shouldTrigger" in props &&
@@ -50,16 +50,18 @@ export function extractEventHandlersFromProps(
                     props[`${event}SpecificIndex`]
                 )
             }
+        }
 
-            if (keyEventTriggerNames.indexOf(event) !== -1) {
-                // preserve onKey* handler overrides
-                handlers[event] = (...args) => {
-                    // execute any existing handlers
-                    if (props[event] && typeof props[event] === "function") {
-                        props[event](...args)
-                    }
+        if (keyEventTriggerNames.indexOf(event) !== -1) {
+            // preserve onKey* handler overrides
+            handlers[event] = (...args) => {
+                // execute any existing handlers
+                if (props[event] && typeof props[event] === "function") {
+                    props[event](...args)
                 }
+            }
 
+            if (action !== "unset") {
                 const hotkey = props[`${event}Key`].trim()
                 if (hotkey !== "") {
                     const options: KeyEventOptions = {
@@ -76,9 +78,9 @@ export function extractEventHandlersFromProps(
                     }
                     keyEvents.push({ hotkey, options, handler })
                 }
-            } else {
-                handlers[event] = handler
             }
+        } else {
+            handlers[event] = handler
         }
         return handlers
     }, {})
