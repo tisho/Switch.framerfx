e4438e93ded10fc7500c4aaeed1eca95bcd88fbc
Tisho Georgiev <tisho@framer.com>
2020-02-04 10:35:11 -0800

    fix: fixed custom easing params not getting parsed correctly

--- code/transitions.ts
+++ code/transitions.ts
@@ -12,6 +12,8 @@ export const transitionOptionsFromProps = (props, prefix = null) => {
             ease:
                 getProp("ease") === "custom"
                     ? getProp("customEase")
+                          .split(/,\s+/)
+                          .map(parseFloat)
                     : getProp("ease"),
         }
     }
