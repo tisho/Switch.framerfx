8a637229118693b9bb7703bdaa925d07343a76ea
Tisho Georgiev <tisho@framer.com>
2020-02-04 11:00:27 -0800

    feat: added more tween transition easing presets

--- code/transitions.ts
+++ code/transitions.ts
@@ -1,6 +1,38 @@
 import { prefixPropName } from "./utils/propNameHelpers"
 import { getOpacity } from "./utils/styleParsing"
 
+const EASINGS = {
+    linear: "linear",
+    easeIn: "easeIn",
+    easeOut: "easeOut",
+    easeInOut: "easeInOut",
+    easeInSine: [0.47, 0, 0.745, 0.715],
+    easeOutSine: [0.39, 0.575, 0.565, 1],
+    easeInOutSine: [0.445, 0.05, 0.55, 0.95],
+    easeInQuad: [0.55, 0.085, 0.68, 0.53],
+    easeOutQuad: [0.25, 0.46, 0.45, 0.94],
+    easeInOutQuad: [0.455, 0.03, 0.515, 0.955],
+    easeInCubic: [0.55, 0.055, 0.675, 0.19],
+    easeOutCubic: [0.215, 0.61, 0.355, 1],
+    easeInOutCubic: [0.645, 0.045, 0.355, 1],
+    easeInQuart: [0.895, 0.03, 0.685, 0.22],
+    easeOutQuart: [0.165, 0.84, 0.44, 1],
+    easeInOutQuart: [0.77, 0, 0.175, 1],
+    easeInQuint: [0.755, 0.05, 0.855, 0.06],
+    easeOutQuint: [0.23, 1, 0.32, 1],
+    easeInOutQuint: [0.86, 0, 0.07, 1],
+    easeInExpo: [0.95, 0.05, 0.795, 0.035],
+    easeOutExpo: [0.19, 1, 0.22, 1],
+    easeInOutExpo: [1, 0, 0, 1],
+    circIn: "circIn",
+    circOut: "circOut",
+    circInOut: "circInOut",
+    backIn: "backIn",
+    backOut: "backOut",
+    backInOut: "backInOut",
+    anticipate: "anticipate",
+}
+
 export const transitionOptionsFromProps = (props, prefix = null) => {
     const getProp = n => props[prefixPropName(n, prefix)]
     const type = getProp("transitionType")
@@ -14,7 +46,7 @@ export const transitionOptionsFromProps = (props, prefix = null) => {
                     ? getProp("customEase")
                           .split(/,\s+/)
                           .map(parseFloat)
-                    : getProp("ease"),
+                    : EASINGS[getProp("ease")],
         }
     }
 
