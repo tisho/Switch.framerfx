594d688d6b155b963779f4c107078c168e962f0f
Adam Seckel <adam.seckel@gmail.com>
2020-09-07 22:51:45 +0100

    Override LayoutIdContext to prevent layoutId generation for descendents

---
+++ code/PreventLayoutIdGeneration.tsx
@@ -0,0 +1,23 @@
+import * as Framer from "framer"
+import * as React from "react"
+
+export function PreventLayoutIdGeneration(props: {
+    children: React.ReactNode | React.ReactNode[]
+}) {
+    const context = React.useRef({
+        getLayoutId: (args) => null,
+        persistLayoutIdCache: () => {},
+        top: false,
+    }).current
+
+    // LayoutIdContext may not exist on Framer if Switch is used in an old version of Framer.
+    // @ts-ignore
+    if (Framer.LayoutIdContext) {
+        return (
+            // @ts-ignore
+            <Framer.LayoutIdContext.Provider value={context} {...props} />
+        )
+    }
+
+    return <>{props.children}</>
+}
